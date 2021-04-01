83936465da673333a039df0a1b85adf4db7890a2
Bárbara Maria Teixeira Costa Peluzo <b211784@dac.unicamp.br>
2021-04-01 16:50:12 -0300

    new file

---
+++ angry_birds.f90
@@ -0,0 +1,6 @@
+ PROGRAM ANGRY_BIRDS
+         IMPLICIT NONE
+         REAL :: x=0.0, y=0.0, v0y=0.0, vx=0.0, vy=0.0
+         REAL, PARAMETER :: h=1, d=100
+         ! REAL, PARAMETER, DIMENSION(2) :: !! colocar aqui as posições dos porquinhos
+         END PROGRAM ANGRY_BIRDS
