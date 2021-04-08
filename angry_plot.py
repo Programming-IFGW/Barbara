4b9c8e42e3e03095ba3741033a49ea71e29ecb67
Bárbara Maria Teixeira Costa Peluzo <b211784@dac.unicamp.br>
2021-04-08 18:08:55 -0300

    corrigi uns detalhes

--- angry_plot.py
+++ angry_plot.py
@@ -5,6 +5,10 @@ import numpy as np
 
 pigs=np.genfromtxt('pigs.dat')
 bird=np.genfromtxt('bird.dat')
+tamanho=np.genfromtxt('param.dat')
+
+x_max=tamanho[0]
+y_max=tamanho[1]
 
 pigs_x=pigs[:,0]
 pigs_y=pigs[:,1]
@@ -13,5 +17,8 @@ bird_x=bird[:,0]
 bird_y=bird[:,1]
 
 plt.figure(1)
-plt.plot(pigs_x,pigs_y,'*')
+plt.xlim(0,1.1*x_max)
+plt.ylim(0,1.1*y_max)
+plt.plot(pigs_x,pigs_y,'*',color='green')
+plt.plot(bird_x,bird_y,'*',color='red')
 plt.show(1)
