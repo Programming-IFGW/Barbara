a8f0392f1f210c4f5bb2c476602e0b46f612cf13
Bárbara Maria Teixeira Costa Peluzo <b211784@dac.unicamp.br>
2021-04-08 17:32:36 -0300

    ESCREVE AS POSIÇÕES DOS PORCOS E PASSAROS

--- angry_birds.f90
+++ angry_birds.f90
@@ -1,11 +1,36 @@
  PROGRAM ANGRY_BIRDS
          IMPLICIT NONE
-         REAL(8) :: x=0.0, y=0.0, v=0.0, angle=0.0, t=0.0, hmax=0.0, d=0.0
-         REAL, PARAMETER :: h0=1, g=9.8, dt=1E-3, siz=0.5
-         INTEGER, PARAMETER :: num_pig=2 
-         INTEGER :: pig_in=1
-         REAL, DIMENSION(2) :: bird=(/0.0,0.0/)
-         REAL, PARAMETER, DIMENSION(2,2) :: pig=RESHAPE((/95,5,98,90/), SHAPE(pig))
+         REAL(8) :: x=0.0, y=0.0, angle=0.0, t=0.0, h_max=0.0, d=0.0, d_max=0.0
+         REAL, PARAMETER :: h0=1, g=9.8, dt=1E-3, siz=0.5, v=100
+         INTEGER :: pig_in=1, num_pig=0
+         REAL, DIMENSION(2) :: bird=(/0.0,0.0/), pig=(/0.0,0.0/)
+        REAL, DIMENSION(:), ALLOCATABLE :: pig_x, pig_y
+
+
+
+        !!!!!!!!!! DISTANCIA MAXIMA DE ALCANCE !!!!!!!!!!!!
+
+        d_max=(2*v**3*SIND(90.0)*COSD(45.0))/(g**2)
+        h_max=h0+((v*COSD(45.0))**2)/(2*g)
+
+       ! PRINT*, d_max
+       ! PRINT*, h_max
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!! SORTEANDO OS PORQUINHOS !!!!!!!!!!!!
+        
+        OPEN(UNIT=15,FILE="pigs.dat",STATUS='REPLACE')
+        PRINT*, "QUANTOS PORQUINHOS?"
+        READ*, num_pig
+        ALLOCATE(pig_x(num_pig))
+        ALLOCATE(pig_y(num_pig))
+
+        DO pig_in=1, num_pig
+                pig_x(pig_in)=d_max*(0.1*RAND()+0.9)
+                pig_y(pig_in)=0.1*h_max*RAND()
+                pig=(/pig_x(pig_in),pig_y(pig_in)/)
+                WRITE(15,*) pig
+        END DO
+
+        !!!!!!!!!!!!!!!!!!!!!!! COMEÇA O JOGO !!!!!!!!!!!!!!!!!!!
 
 1       PRINT*, "QUAL O ANGULO?"
         READ*, angle
@@ -13,19 +38,19 @@
                 PRINT*, "ANGULO ERRADO!!!"
                 GOTO 1
         END IF
+        
+        OPEN(UNIT=16,FILE="birds.dat",STATUS='REPLACE')
        
-        PRINT*, "QUAL O VALOR DA VELOCIDADE INICIAL?"
-        READ*, v
-
-        hmax=h0+((v*COSD(angle))**2)/(2*g)
+      !  hmax=h0+((v*COSD(angle))**2)/(2*g)
       !  v0y=v*COSD(angle)
       !  vx=v*SIND(angle)
         d=(2*v**3*SIND(2*angle)*COSD(angle))/(g**2)
         DO WHILE (x .LE. d) 
                 DO pig_in=1, num_pig
                       !  PRINT*, SQRT(DOT_PRODUCT(bird-pig(:,pig_in),bird-pig(:,pig_in)))
-                        PRINT*, bird
-                        IF (SQRT(DOT_PRODUCT(bird-pig(:,pig_in),bird-pig(:,pig_in))) <= siz) THEN
+                        WRITE(16,*) bird
+                        pig=(/pig_x(pig_in),pig_y(pig_in)/)
+                        IF (SQRT(DOT_PRODUCT(bird-pig,bird-pig)) <= siz) THEN
                         PRINT*, "GANHOU :)"
                         EXIT       
                         END IF
@@ -37,7 +62,8 @@
         END DO
         PRINT*, "PERDEU :("
 
-
+        CLOSE(UNIT=15)
+        CLOSE(UNIT=16)
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
         CONTAINS
