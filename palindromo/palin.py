f252a29e08bcfd904397d5a56cb45f391159fee0
Bárbara Maria Teixeira Costa Peluzo <b211784@dac.unicamp.br>
2021-04-29 18:35:38 -0300

    conseguimos achar palindromos de 3 letras!!

--- palindromo/palin.py
+++ palindromo/palin.py
@@ -1,10 +1,17 @@
 word=input("Digite uma palavra:")
 size=len(word)
 
+
+n_palin=0
 for i in range(size):
     for j in range(i+1,size):
-        print(i)
-        print(j)
-        print("%s %s", % word[i], word[j])
-            #if word[i] == word[j]:
-             #   print("letras iguais")
+       # print("%s %s" %( word[i], word[j]))
+        if word[i] == word[j]:
+            word2=word[i+1:j]
+            if len(word2) == 1:
+                print("Achei um palindromo!: %s" %word[i:j+1])
+                n_palin=n_palin+1
+
+
+print("Temos %d palindromos!!" % n_palin)
+
